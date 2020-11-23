ee6b12170e9f88ae5d8d14becc4f61b52a7176ff
yann_djamen <yann_djamen@groupevsc.com>
2020-11-23 15:18:20 +0100

    adding the getAllproducts and orderProductById functions

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 var fs = require('fs');
 var path = require('path');
 var filename = "products.json";
+var readline = require('readline');
+var rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: false});
 
-fs.readFile(path.join(__dirname, filename) , (err, data) => {
-    if (err) {
-        console.error(err);
-        return
-    }
-    var books = JSON.parse(data);
-    console.log(books)
+function getAllProducts() {
+    fs.readFile(path.join(__dirname, filename), (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        console.log("Bienvenue, voici les produits disponibles");
+        try {
+            var books = JSON.parse(data);
+            books.forEach((book) => {
+                console.log(" X " + book.id + " - " + book.name + " / " + book.EUR_price + "€ / " + book.orders_counter);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    });
+}
+
+function orderProductById(id) {
+    fs.readFile(path.join(__dirname, filename), (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        try {
+            var books = JSON.parse(data);
+            var bookUrl = "";
+            books.map((book) => {
+                if (id === book.id) {
+                    book.orders_counter = book.orders_counter + 1;
+                    bookUrl = book.file_link;
+                    return book;
+                }
+            });
+
+            var data = JSON.stringify(books, null, 2);
+
+            fs.writeFile(path.join(__dirname, filename), data, function (err) {
+                if (err) return console.log(err);
+                console.log('Commande terminée, voici votre fichier', bookUrl);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    });
+}
+
+getAllProducts();
+rl.on('line', function (line) {
+    console.log('I want ', line.toString());
+    orderProductById(line.toString());
 });
